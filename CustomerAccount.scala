842425e629bf2a5b349f4cc903bb4d9b9f0e7c02
bjwealthy <bjwealthy@gmail.com>
2018-04-14 12:42:52 +0100

    second commit

--- CustomerAccount.scala
+++ CustomerAccount.scala
@@ -1,20 +1,37 @@
 import java.util._
 import java.time.LocalDate
 
-object CustomerAccount extends App {
+object Account extends App {
   val accountCounter: AtomicInteger = new AtomicInteger(0)
 
-  case class AccountPresentState(amountDeposied: Double, balance: Double, date: LocalDate)
+  case class AccountPresentState(date: LocalDate, openingAmount:Double, balance:Double)
 
-  def createAccount(): Account = CustomerAccount(0, accountCounter.getAndIncrement(), List())
+  def createAccount(): Account = Account(0, accountCounter.getAndIncrement(), List.empty())
 
 }
 
-  case class CustomerAccount(balance: Double, PresentState: List[CustomerAccount.AccountPresentState], id: Int){
-    def changeBalance(amountDeposited) : Account = {
-      val newBalance = (balance+amountDeposited)
-
+  case class Account(balance: Double, PresentState: List[Account.AccountPresentState], id: Int){
+    def changeBalance(openingAmount) : Account = {
+      val newBalance = (balance + openingAmount)
+      Account(newBalance, Account.AccountPresentState(LocalDate.now(), openingAmount, balance))
   }
+
+    def deposit(amountDepWithd: Double): Account = {
+      if (amountDepWithd > 0) changeBalance(amountDepWithd)
+      else this
+    }
+
+    def withdrawal(amountDepWithd: Double) : Acount = {
+      if(balance<amountDepWithd || amountDepWithd <= 0)
+        (this, 0)
+      else
+        changeBalance(-amountDepWithd)
+    }
+
+    def printStatement() ={
+      println("Below is the your acct statement. in the order of Date OpeningAmount Balance")
+      PresentState.forEach(acct => println(s"$acct.date $acct.openingAmount $acct.balance"))
+    }
 }
 
 
